23c2f9f78dc9deba2761b65f80abb18f80c9bd59
SahasT23 <sahas.st01@gmail.com>
2025-06-12 18:49:18 +0100

    Added all of the files for Q set one. Need to add the answers as well

---
+++ CLongFormSolutions/Set 1/Q1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char operator;
+
+    // Read input from user
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+    
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    // Perform calculation based on operator
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+            
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+            
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+            
+        case '/':
+            if(num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Error: Division by zero is not allowed!\n");
+            }
+            break;
+            
+        default:
+            printf("Error: Invalid operator!\n");
+    }
+
+    return 0;
+}
