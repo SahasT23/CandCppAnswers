d995a86e631538937dd3585759a3bbbbe4edf90e
SahasT23 <sahas.st01@gmail.com>
2025-06-12 19:30:21 +0100

    All of C set 2 done

---
+++ CLongFormSolutions/Set 2/Q10.c
@@ -0,0 +1,48 @@
+// String conceatenation method
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *concatStrings(const char *s1, const char *s2) {
+    char *result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+    if (!result) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+    
+    char *temp = result;
+    while (*s1) {
+        *temp++ = *s1++;
+    }
+    while (*s2) {
+        *temp++ = *s2++;
+    }
+    *temp = '\0';
+    
+    return result;
+}
+
+int main() {
+    char s1[100], s2[100];
+    
+    printf("Enter first string: ");
+    fgets(s1, sizeof(s1), stdin);
+    if (s1[strlen(s1) - 1] == '\n') {
+        s1[strlen(s1) - 1] = '\0';
+    }
+    
+    printf("Enter second string: ");
+    fgets(s2, sizeof(s2), stdin);
+    if (s2[strlen(s2) - 1] == '\n') {
+        s2[strlen(s2) - 1] = '\0';
+    }
+    
+    char *result = concatStrings(s1, s2);
+    if (result) {
+        printf("Concatenated string: %s\n", result);
+        free(result);
+    }
+    
+    return 0;
+}
---
+++ CLongFormSolutions/Set 2/Q9.c
@@ -0,0 +1,32 @@
+// Memory Allocation Checker
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Error: No numbers provided!\n");
+        return 1;
+    }
+    
+    int *numbers = (int *)malloc((argc - 1) * sizeof(int));
+    if (!numbers) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+    
+    int sum = 0;
+    for (int i = 1; i < argc; i++) {
+        *(numbers + (i - 1)) = atoi(argv[i]);
+        sum += *(numbers + (i - 1));
+    }
+    
+    printf("Numbers: ");
+    for (int i = 0; i < argc - 1; i++) {
+        printf("%d ", *(numbers + i));
+    }
+    printf("\nSum: %d\n", sum);
+    
+    free(numbers);
+    return 0;
+}
