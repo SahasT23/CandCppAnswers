b44a851f9827a9b9b800d7efaaadb7e32bb23ff7
SahasT23 <sahas.st01@gmail.com>
2025-06-12 19:19:36 +0100

    Added Q4.c for set 1

---
+++ CLongFormSolutions/Set 1/Q4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Global variable
+int globalCounter = 0;
+
+void incrementGlobal() {
+    // Modify global variable
+    globalCounter++;
+    printf("In incrementGlobal: globalCounter = %d\n", globalCounter);
+}
+
+void localCounterFunction() {
+    // Local variable
+    int localCounter = 0;
+    localCounter++;
+    printf("In localCounterFunction: localCounter = %d, globalCounter = %d\n", 
+           localCounter, globalCounter);
+}
+
+int main() {
+    // Initial state
+    printf("Initial: globalCounter = %d\n", globalCounter);
+    
+    // Call functions multiple times to demonstrate
+    incrementGlobal();
+    localCounterFunction();
+    incrementGlobal();
+    localCounterFunction();
+    
+    // Final state
+    printf("Final: globalCounter = %d\n", globalCounter);
+    
+    return 0;
+}
